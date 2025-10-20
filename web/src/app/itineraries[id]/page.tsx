c7395ff065a34805ce8ae0b02d915a80c26ddd92
WingedManiac <159825481+WingedManiac@users.noreply.github.com>
2025-10-20 10:51:09 -0500

    scaffold for itineraries

---
+++ web/src/app/itineraries[id]/page.tsx
@@ -0,0 +1,15 @@
+/*function itineraryDetailPage(){
+}*/
+
+/*function loadItems(tripId:Id){
+}*/
+
+/*funtion handleItems(input:AddItemInput){
+}*/
+
+/*function handleUpdateItem(itemId:Id, patch:UpdateItemInput){
+}*/
+
+/*function handleRemoveItem(itemId:Id){
+}*/
+
