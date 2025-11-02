6e76e1f577621aa4073f8e0f7266fff6fb8708ad
DustinWZook <dustin.w.zook@gmail.com>
2025-11-02 16:00:06 -0600

    redeploy

---
+++ web/src/app/api/env-check/route.ts
@@ -0,0 +1,6 @@
+export async function GET() {
+  return Response.json({
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: !!process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    GOOGLE_MAPS_SERVER_KEY: !!process.env.GOOGLE_MAPS_SERVER_KEY,
+  });
+}
